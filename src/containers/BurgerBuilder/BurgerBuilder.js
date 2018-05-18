88b1e99563d691769835e2d5c87ef2f5b919e6ab
RedLeader12 <redleader@jigsaw.xyz>
2018-05-18 09:01:29 +0800

    Created BurgerBuilder as a potential stateful component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Aux from '../../HOC/Aux'
+
+class BurgerBuilder extends Component {
+    render(){
+        return(
+            <Aux> 
+            <p> BuildControl </p> 
+            <p> Burger </p> 
+            </Aux> 
+        );
+    }
+}
+
+export default BurgerBuilder 
