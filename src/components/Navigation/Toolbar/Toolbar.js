7cc5f2ff26bdcc0ddffd66c089246c5c15fb4065
RedLeader12 <redleader@jigsaw.xyz>
2018-05-21 18:49:35 +0800

    Made the sideDrawer dynamic with the logo using css styling, and some inline styling to pass the height of the logo as props

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,10 +4,13 @@ import classes from './Toolbar.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems';
 
+
 const toolbar = (props) => (
     <header className={classes.Toolbar}> 
         <div> MENU </div> 
-        < Logo /> 
+        <div className={classes.Logo}> 
+        < Logo/> 
+        </div> 
         <nav>
             <NavigationItems/> 
         </nav> 
