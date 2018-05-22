837927421e8341121bdc1c68455193532683bacb
RedLeader12 <redleader@jigsaw.xyz>
2018-05-22 11:16:22 +0800

    Placed componentwillUpdate in modal to prevent ordersummary from rendering when its not shown or changed

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, {Component}from 'react';
 
 import Aux from '../../../HOC/Aux';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
+    //This could be a functional component. This is for debuggign
+    componentWillUpdate(){
+        console.log('[OrderSummary] Will Update')
+    }
 
-    const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey =>{
-        return <li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li> 
-    });
+    render(){
 
-    return (
-        <Aux>
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey =>{
+            return <li key={igKey}>
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li> 
+        });
+
+        return(
+            <Aux>
             <h3> Your Order </h3> 
             <p> Your delicious burger bill </p> 
             <ul> 
                 {ingredientSummary}  
             </ul> 
-            <p><strong> Total Price: $ {props.price.toFixed(2)}</strong> </p> 
+            <p><strong> Total Price: $ {this.props.price.toFixed(2)}</strong> </p> 
             <p> Continue to Checkout ? </p> 
-            <Button btnType="Danger" clicked={props.purchaseCanceled}> CANCEL </Button> 
-            <Button btnType="Success" clicked={props.purchaseContinued}> CONTINUE </Button> 
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}> CANCEL </Button> 
+            <Button btnType="Success" clicked={this.props.purchaseContinued}> CONTINUE </Button> 
         </Aux> 
-    )
-};
+        )
+    }
+}
 
-export default orderSummary; 
+export default OrderSummary
