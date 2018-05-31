615058bbb3d4b1c55faaaff05060fd69ec4cd7eb
RedLeader12 <redleader@jigsaw.xyz>
2018-05-31 17:17:53 +0800

    Created a dropdown box where it dynamically accepts the options of fastest and cheapest

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -8,19 +8,33 @@ const input = (props) => {
 
     switch(props.elementType){
         case('input'): 
-            inputElement = <input className={classes.InputElement}  
+            inputElement = <input 
+                                  className={classes.InputElement}  
                                   {...props.elementConfig} 
-                                  value={props.value}/>
+                                  value={props.value}
+                            />
             break; 
         case('textarea'):
-            inputElement = <textarea className={classes.InputElement} 
-                                     {...props.elementConfig}  
-                                     value={props.value} /> 
+            inputElement = <textarea  
+                                    className={classes.InputElement} 
+                                    {...props.elementConfig}  
+                                    value={props.value} 
+                            /> 
             break; 
+            case('select'): 
+            inputElement = <select 
+                                className={classes.InputElement}  
+                                value={props.value}>
+                            {props.elementConfig.options.map(option => (
+                                <option key={option.value} value={option.value}>{option.display}</option> 
+                            ))}
+                            </select> 
+            break;
         default: 
         inputElement = <input className={classes.InputElement} 
                               {...props.elementConfig} 
-                              value={props.value}/> 
+                              value={props.value}
+                        /> 
     }
 
     return (
